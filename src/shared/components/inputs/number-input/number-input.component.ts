920bf9455546b9696cca1b8b74e9c966960d9215
ginoLanetti <alfonsichqu@gmail.com>
2019-12-22 10:49:48 +0100

    WIP

--- src/shared/components/inputs/number-input/number-input.component.ts
+++ src/shared/components/inputs/number-input/number-input.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class NumberInputComponent implements OnInit {
   @Input() infoText: string;
+  @Input() placeholder: string;
 
   constructor() { }
 
